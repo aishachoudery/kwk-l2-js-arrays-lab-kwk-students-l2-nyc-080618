6a5a51161ceae94757fdd721277b1be59dd90e0f
aishachoudery <aichoudery31@gmail.com>
2018-08-08 20:09:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 const drivers = ["Milo", "Otis", "Garfield"]
 //drivers.push("Ralph")
 function destructivelyAppendDriver(name) {
-  drivers.push("Ralph")
+  drivers.push("Ralph");
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.shift();
 }
